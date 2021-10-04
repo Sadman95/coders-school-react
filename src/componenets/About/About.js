61289bcceffa80d165ae55d13acd0d54fcb7b511
Sadman95 <mail.sakib.1995@gmail.com>
2021-10-04 09:51:08 +0600

    about done

--- src/componenets/About/About.js
+++ src/componenets/About/About.js
@@ -1,10 +1,22 @@
+import { Box } from '@mui/system';
 import React from 'react';
+import about from './about.jpg';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 
 const About = () => {
     return (
-        <div>
-            <h1>This is about us</h1>
-        </div>
+        <Box sx={{display: 'flex',justifyContent: 'space-evenly', alignItems: 'center'}}>
+            <Box sx={{textAlign: 'left'}}>
+                <h1 className='spanned'>About Us</h1>
+                <p><CheckCircleOutlineIcon></CheckCircleOutlineIcon>&nbsp;We provide good quality tutorials with realistic example</p>        
+                <p><CheckCircleOutlineIcon></CheckCircleOutlineIcon>&nbsp;We have skilled trainers.</p>
+                <p><CheckCircleOutlineIcon></CheckCircleOutlineIcon>&nbsp;We organize special bootcamp sessions.</p>
+                <p><small>and many more...</small></p>
+            </Box>
+            <Box sx={{width: '50%'}}>
+                <img style={{width: '100%'}} src={about} alt="" />
+            </Box>
+        </Box>
     );
 };
 
