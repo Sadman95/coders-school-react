2726b42264bc3f56edbacad32d5725c8a7767fc0
Sadman95 <mail.sakib.1995@gmail.com>
2021-10-04 22:54:27 +0600

    enrolled text styled

--- src/componenets/Enrolled/Enrolled.js
+++ src/componenets/Enrolled/Enrolled.js
@@ -1,9 +1,20 @@
+import { Typography } from '@mui/material';
+import { Box } from '@mui/system';
 import React from 'react';
 const Enrolled = () => {
+    const textStyle= {
+        display: 'inline-block',
+        padding: 2,
+        margin: 2,
+        border: 1,
+        borderRadius: 1,
+        borderColor: 'grey.500'}
     return (
-        <div>
-            <h1>Succesfully enrolled!!!</h1>
-        </div>
+        <Box>
+            <Typography sx={textStyle} variant="h3">
+                Succesfully enrolled!!!
+            </Typography>
+        </Box>
     );
 };
 
